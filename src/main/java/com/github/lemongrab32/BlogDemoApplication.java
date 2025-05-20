20cfed0ab7603aad5c6bc6d8186e0ace5441b19a
IO3683 <nu-dopustim@mail.ru>
2025-05-20 16:20:31 +0600

    initial commit

---
+++ src/main/java/com/github/lemongrab32/BlogDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.lemongrab32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogDemoApplication.class, args);
+    }
+
+}
